392f86ffabd353c1c4056476525f733bb2198312
Vijay Aravamudhan <avijayr - at - gmail - dot - com>
2014-07-26 21:00:26 +0530

    Added validations for dictionary model. Converted application.css to be pre-processed via the scss preprocessor by adding the requisite extension. Converted all pages to use the bootstrap3 ui.

--- app/models/dictionary.rb
+++ app/models/dictionary.rb
@@ -1,5 +1,5 @@
 class Dictionary < ActiveRecord::Base
   default_scope { order(word: :asc) }
 
-  validates :word,:meaning, allow_blank: false, :uniqueness => {case_sensitive: false}
+  validates :word, :meaning, allow_blank: false, :uniqueness => {case_sensitive: false}
 end
