285f84e6d381cf1a17d7860ac85b108941ad5b7c
Aravind <pro.aravind@gmail.com>
2014-07-26 14:48:57 +0530

    added ajax pagination and fake data for dictionary

--- app/models/dictionary.rb
+++ app/models/dictionary.rb
@@ -1,4 +1,5 @@
 class Dictionary < ActiveRecord::Base
   default_scope { order(word: :asc) }
-  validates :word, :meaning, allow_blank: false
+
+  validates :word,:meaning, allow_blank: false, :uniqueness => {case_sensitive: false}
 end
